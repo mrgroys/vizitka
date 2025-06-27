6a8063f870da56fb3abc02ade06b90cdf28b7116
Yuliya Kleschevich <ruygaem@gmail.com>
2025-06-27 15:29:30 +0300

    убран лишний events из админки

--- frontend/src/app/admin-panel/page.js
+++ frontend/src/app/admin-panel/page.js
@@ -10,13 +10,6 @@ import { events, dressCodeColors } from '@/utils/constants';
 import styles from './page.module.css';
 
 export default function Admin() {
-  const [events, setEvents] = useState([
-    { time: "16:00", item: "Сбор гостей", address: "ул. Ставропольская 123", color: "#DCC086" },
-    { time: "17:00", item: "Выездная церемония", address: "ул. Павлова 3", color: "#DCC086" },
-    { time: "17:30", item: "Праздничный ужин", address: "ул. Пушкина 5", color: "#DCC086" },
-    { time: "23:30", item: "Завершение торжества", address: "", color: "#DCC086" }
-  ]);
-
   return (
     <div className={styles.page}>
       <Header
